d20b781a819ebbd609f18b40dcba2d3a9b345e0f
Richard <taerichard@gmail.com>
2020-06-12 03:09:10 -0700

    creating trainer services, adding location

---
+++ server/PersonalTrainer/PersonalTrainer/Repositories/ITrainerRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PersonalTrainer.Models;
+
+namespace PersonalTrainer.Repositories
+{
+    internal interface ITrainerRepository
+    {
+        Trainer Post(Trainer trainer);
+
+        void Delete(int id);
+
+        IEnumerable<Trainer> GetTrainers();
+
+        Trainer GetTrainer(Trainer trainer);
+
+        void Edit(int id);
+    }
+}
---
+++ server/PersonalTrainer/PersonalTrainer/Repositories/TrainerRepository.cs
@@ -0,0 +1,52 @@
+﻿using PersonalTrainer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PersonalTrainer.Data;
+
+namespace PersonalTrainer.Repositories
+{
+    public class TrainerRepository : ITrainerRepository
+    {
+        private readonly TrainerContext _trainerContext;
+
+        public TrainerRepository(TrainerContext trainerContext)
+        {
+            _trainerContext = trainerContext;
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Trainer GetTrainer(Trainer trainer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Trainer> GetTrainers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Trainer Post(Trainer trainer)
+        {
+            Trainer newTrainer = new Trainer
+            {
+                FirstName = trainer.FirstName,
+                LastName = trainer.LastName,
+                Email = trainer.Email,
+                Location = trainer.Location
+            };
+
+            return newTrainer;
+        }
+    }
+}
