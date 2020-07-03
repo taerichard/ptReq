f3433f1f3848773f39c7220a4ae8edbc21ac2984
Richard <taerichard@gmail.com>
2020-07-02 22:42:33 -0700

    seeded database

---
+++ server/PersonalTrainer/PersonalTrainer/Data/DbInitializer.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Bogus;
+using Microsoft.EntityFrameworkCore;
+using PersonalTrainer.Controllers;
+using PersonalTrainer.Models;
+
+namespace PersonalTrainer.Data
+{
+    public class DbInitializer
+    {
+        private TrainerContext _trainerContext;
+
+        public DbInitializer(TrainerContext trainerContext)
+        {
+            _trainerContext = trainerContext;
+        }
+
+        public void SeedData()
+        {
+            if (_trainerContext.TrainerLocations.Any())
+            {
+                List<TrainerLocation> trainerLocationList = new List<TrainerLocation>();
+
+                for (int i = 0; i < 30; i++)
+                {
+                    var trainer = GenerateTrainer();
+                    var location = GenerateLocation();
+                    var trainerLocation = new TrainerLocation
+                    {
+                        Trainer = trainer,
+                        Location = location
+                    };
+
+                    //context.Trainers.Add(trainer);
+                    //context.Locations.Add(location);
+                    trainerLocationList.Add(trainerLocation);
+                }
+
+                _trainerContext.TrainerLocations.AddRange(trainerLocationList);
+                _trainerContext.SaveChanges();
+            }
+        }
+
+        public static TrainerLocation CreateTrainerLocation(Trainer trainer, Location location)
+        {
+            return new TrainerLocation
+            {
+                Location = location,
+                Trainer = trainer,
+            };
+        }
+
+        public static Location GenerateLocation()
+        {
+            var fakeLocation = new Faker<Location>()
+                .RuleFor(l => l.City, c => c.Address.City())
+                .RuleFor(s => s.State, c => c.Address.State());
+
+            return fakeLocation;
+        }
+
+        public static Trainer GenerateTrainer()
+        {
+            var fakeTrainer = new Faker<Trainer>()
+                 .RuleFor(f => f.FirstName, c => c.Name.FirstName())
+                 .RuleFor(l => l.LastName, c => c.Name.LastName())
+                 .RuleFor(e => e.Email, (f, v) => f.Internet.Email(v.FirstName, v.LastName));
+
+            return fakeTrainer;
+        }
+    }
+}
--- server/PersonalTrainer/PersonalTrainer/Startup.cs
+++ server/PersonalTrainer/PersonalTrainer/Startup.cs
@@ -17,15 +17,16 @@ namespace PersonalTrainer
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<DbInitializer>();
             services.AddControllers();
             services.AddTransient<ITrainerLocationRepository, TrainerLocationRepository>();
             services.AddTransient<ITrainerRepository, TrainerRepository>();
@@ -35,7 +36,7 @@ namespace PersonalTrainer
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, DbInitializer dataSeeder)
         {
             if (env.IsDevelopment())
             {
@@ -43,6 +44,8 @@ namespace PersonalTrainer
             }
 
             app.UseRouting();
+
+            dataSeeder.SeedData();
 
             //app.UseAuthentication();
 
